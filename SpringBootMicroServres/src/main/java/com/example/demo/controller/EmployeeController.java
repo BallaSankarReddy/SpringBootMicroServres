cf36a8d74cd6de5356111b38ff70f83ea855dbf3
BallaSankarReddy <ballasankarreddy@gmail.com>
2019-09-07 00:11:00 +0530

    Added new packages and classes

---
+++ SpringBootMicroServres/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,46 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@GetMapping("/employees")
+	public List<Employee> getAllEmlEmployees() {
+		return employeeRepository.findAll();
+	}
+
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId)
+			throws ResourceNotFoundException {
+		Employee employee = employeeRepository.findByEmpid(employeeId);
+		// Optional<Employee> emp.employee.orElseThrow(() -> new
+		// ResourceNotFoundException("Employee not found for this id :: " +
+		// employeeId));
+		return ResponseEntity.ok().body(employee);
+	}
+
+	@PostMapping("/employees")
+	public Employee createEmployee(@Valid @RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
+}
---
+++ SpringBootMicroServres/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,58 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employees")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long empid;
+	private String firstName;
+	private String lastName;
+	private String emailId;
+
+	public Employee() {
+	}
+
+	public long getEmpid() {
+		return empid;
+	}
+
+	public void setEmpid(long empid) {
+		this.empid = empid;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
+	
+	
+	
+}
