74f0d70a34105a49dfdf603975fa608fc4864439
BallaSankarReddy <ballasankarreddy@gmail.com>
2019-09-07 14:16:19 +0530

    New Code added

--- SpringBootMicroServres/src/main/java/com/example/demo/controller/Democontroller.java
+++ SpringBootMicroServres/src/main/java/com/example/demo/controller/Democontroller.java
@@ -5,16 +5,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 
+@Slf4j
 public class Democontroller {
 
-	 private static final Logger logger = LoggerFactory.getLogger(Democontroller.class);
-	 
+	private static final Logger log = LoggerFactory.getLogger(Democontroller.class);
+
 	@GetMapping("/display")
 	public String display() {
-		logger.info("Calling from Get Request ...Methode..!");
-		//System.out.println("Calling from Get Request ...Methode..!");
+		log.info("Calling from Get Request ...Methode..!");
+		// System.out.println("Calling from Get Request ...Methode..!");
 		return "Hello Spring Boot MicroService Controller...";
 	}
 }
--- SpringBootMicroServres/src/main/java/com/example/demo/controller/EmployeeController.java
+++ SpringBootMicroServres/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,25 +20,33 @@ import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Employee;
 import com.example.demo.repository.EmployeeRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class EmployeeController {
+
+	private static final Logger log = LoggerFactory.getLogger(EmployeeController.class);
 
 	@Autowired
 	private EmployeeRepository employeeRepository;
 
+	
+
 	@GetMapping("/employees")
 	public List<Employee> getAllEmlEmployees() {
+		log.info("Calling Get Method of List of employees...."+employeeRepository.findAll());
 		return employeeRepository.findAll();
 	}
 
 	@GetMapping("/employees/{id}")
 	public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId)
 			throws ResourceNotFoundException {
-		Employee employee = employeeRepository.findByEmpid(employeeId);
-		// Optional<Employee> emp.employee.orElseThrow(() -> new
-		// ResourceNotFoundException("Employee not found for this id :: " +
-		// employeeId));
+		//Employee employee = employeeRepository.findByEmpid(employeeId);
+		Employee employee = Optional.of(employeeRepository.findByEmpid(employeeId))
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+		log.info("Employee details based on empid...!"+"EmpId"+employeeId+"--->"+employee.getFirstName()+"::"+employee.getLastName());
 		return ResponseEntity.ok().body(employee);
 	}
 
