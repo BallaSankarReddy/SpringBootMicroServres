8f9607590cb9febc1b1450e88b383bd22e184b1e
BallaSankarReddy <ballasankarreddy@gmail.com>
2019-09-14 12:03:21 +0530

    aaa

--- SpringBootMicroServres/src/main/java/com/example/demo/repository/EmployeeAccountDetails.java
+++ SpringBootMicroServres/src/main/java/com/example/demo/repository/EmployeeAccountDetails.java
@@ -1,10 +1,11 @@
 
   package com.example.demo.repository;
   
-  import org.springframework.data.jpa.repository.JpaRepository; import
+  import org.springframework.data.jpa.repository.JpaRepository;
+import
   org.springframework.stereotype.Repository;
-  
-  import com.model.address.empmodel.EmployeeeAccountDetails;
+
+import com.model.address.empmodel.EmployeeeAccountDetails;
   
   @Repository public interface EmployeeAccountDetails extends
   JpaRepository<EmployeeeAccountDetails, Long>{
